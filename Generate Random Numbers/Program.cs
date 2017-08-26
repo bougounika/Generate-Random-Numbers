b92f5000c455584a0d90fe202ef4b3bcea0b35cc
bougounika <31166561+bougounika@users.noreply.github.com>
2017-08-26 12:05:24 +0400

    Add project files.

---
+++ Generate Random Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * C# Program to Generate Random Numbers
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generate_Random_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Some Random Numbers that are generated are : ");
+            for (int i = 1; i < 10; i++)
+            {
+                Randfunc();
+            }
+        }
+        static Random r = new Random();
+        static void Randfunc()
+        {
+            int n = r.Next();
+            Console.WriteLine(n);
+            Console.ReadLine();
+
+        }
+    }
+}
